f3fff18652c47738fc44ebf85e794c70eaa6f539
taufiqurSabbir <taufiqur.sabbir11@gmail.com>
2023-09-24 21:58:18 +0600

    project ini

---
+++ app/Http/Controllers/Api/User/AuthController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function test(){
+        return 'bobo';
+    }
+}
