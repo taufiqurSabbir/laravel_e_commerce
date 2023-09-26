7e08a05dd29e6136c6d4d7c83fe5b28bc84230bc
Taufiqur Rahman <62732164+taufiqurSabbir@users.noreply.github.com>
2023-09-26 14:54:05 +0600

    user info

--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/login',[AuthController::class,'login']);
 Route::post('/register',[AuthController::class,'Register']);
 Route::post('/guest/login',[AuthController::class,'GuestLogin']);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+Route::post('/logout',[AuthController::class,'logout']);
+Route::get('/user/info',[AuthController::class,'userInfo']);
+});
