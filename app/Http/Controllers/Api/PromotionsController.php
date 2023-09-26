3d473f5b582da85eeb0c542a16105c394355a938
Taufiqur Rahman <62732164+taufiqurSabbir@users.noreply.github.com>
2023-09-26 19:46:00 +0600

    slider add function implement

--- app/Http/Controllers/Api/PromotionsController.php
+++ app/Http/Controllers/Api/PromotionsController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\Slider\SliderRequest;
+use App\Models\Promotions;
 
 class PromotionsController extends Controller
 {
   public function addslider(SliderRequest $request){
+      $image_name = rand().'.'.$request->slider_image->extension();
+      request('slider_image') ->move('image/slider',$image_name);
 
-
+    Promotions::create([
+        'image'=>$image_name
+    ]);
+    return send_massage('slider added',true,200);
   }
 }
