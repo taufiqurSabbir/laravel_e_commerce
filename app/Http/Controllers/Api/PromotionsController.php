9d5a5e5746b3b078daee5f5c858f9a92997cca85
Taufiqur Rahman <62732164+taufiqurSabbir@users.noreply.github.com>
2023-09-26 19:33:54 +0600

    slider

--- app/Http/Controllers/Api/PromotionsController.php
+++ app/Http/Controllers/Api/PromotionsController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Requests\Slider\SliderRequest;
+
 class PromotionsController extends Controller
 {
-    //
+  public function addslider(SliderRequest $request){
+
+
+  }
 }
