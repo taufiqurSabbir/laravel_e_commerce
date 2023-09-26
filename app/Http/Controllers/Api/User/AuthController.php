2b4403c110ced34d3126288ecec343e032d4cae4
Taufiqur Rahman <62732164+taufiqurSabbir@users.noreply.github.com>
2023-09-26 14:52:38 +0600

    helper return response function

--- app/Http/Controllers/Api/User/AuthController.php
+++ app/Http/Controllers/Api/User/AuthController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Api\User;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\Api\Controller;
 use App\Http\Requests\User\GuestLoginRequest;
 use App\Http\Requests\User\loginRequest;
 use App\Http\Requests\User\RegistationRequest;
@@ -10,8 +10,6 @@ use App\Http\Resources\User\AuthResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\ValidationException;
 
 
 class AuthController extends Controller
@@ -81,6 +79,17 @@ class AuthController extends Controller
             'token_type' => 'Bearer'
         ]]);
 
+    }
 
+    public function logout(Request $request){
+           $request->user()->tokens()->delete();
+
+           return send_massage('logout Successful',true,200);
+
+    }
+
+    public function userInfo(Request $request){
+        $user = $request->user();
+        return send_massage($user,true,200);
     }
 }
