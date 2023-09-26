4b5a0d45e47de332c9fac06de9f14ea03c818556
Taufiqur Rahman <62732164+taufiqurSabbir@users.noreply.github.com>
2023-09-26 19:32:44 +0600

    order adjust

--- database/migrations/2023_09_25_154558_create_products_table.php
+++ database/migrations/2023_09_25_154558_create_products_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->String('product_code');
             $table->String('name');
             $table->String('description');
             $table->json('color');
--- database/migrations/2023_09_25_154914_create_orders_table.php
+++ database/migrations/2023_09_25_154914_create_orders_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->foreignId('product_id')->constrained('products');
             $table->String('quantity');
             $table->String('shipping_address');
+            $table->String('total_price');
             $table->timestamps();
         });
     }
